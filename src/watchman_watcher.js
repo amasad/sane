461634bce26a8a635c524d5f7c33a3a9ccdd2e5d
François Voron <fvoron@wuha.io>
2018-01-20 19:09:25 +0100

    Allow to set a custom watchman binary path

--- src/watchman_watcher.js
+++ src/watchman_watcher.js
@@ -53,7 +53,9 @@ WatchmanWatcher.prototype.init = function() {
   }
 
   var self = this;
-  this.client = new watchman.Client();
+  this.client = new watchman.Client(
+    this.watchmanPath ? { watchmanBinaryPath: this.watchmanPath } : {}
+  );
   this.client.on('error', function(error) {
     self.emit('error', error);
   });
