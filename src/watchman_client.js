8be8b9689d5bd22b936488e593564758601cdb8b
Stefan Penner <stefan.penner@gmail.com>
2018-02-06 16:08:28 -0800

    Fix issues: (#111)
    * formatting
    * drop features not supported by node 4

--- src/watchman_client.js
+++ src/watchman_client.js
@@ -13,39 +13,40 @@ var watchman = require('fb-watchman');
  * we're using 'watch-project' or 'watch', what the 'project root' is when
  * we internally use watch-project, whether a connection has been lost
  * and reestablished, etc. Also switched to doing things with promises and known-name
- * methods in WatchmanWatcher, so as much information as possible can be kept in 
+ * methods in WatchmanWatcher, so as much information as possible can be kept in
  * the WatchmanClient, ultimately making this the only object listening directly
- * to watchman.Client, then forwarding appropriately (via the known-name methods) to 
+ * to watchman.Client, then forwarding appropriately (via the known-name methods) to
  * the relevant WatchmanWatcher(s).
- * 
+ *
  * Note: WatchmanWatcher also added a 'watchmanPath' option for use with the sane CLI.
  * Because of that, we actually need a map of watchman binary path to WatchmanClient instance.
  * That is set up in getInstance(). Once the WatchmanWatcher has a given client, it doesn't
  * change.
  *
  * @class WatchmanClient
- * @param String watchmanBinaryPath 
+ * @param String watchmanBinaryPath
  * @public
  */
 
 function WatchmanClient(watchmanBinaryPath) {
-
   // define/clear some local state. The properties will be initialized
   // in _handleClientAndCheck(). This is also called again in _onEnd when
   // trying to reestablish connection to watchman.
-  this._clearLocalVars(); 
-  
+  this._clearLocalVars();
+
   this._watchmanBinaryPath = watchmanBinaryPath;
 
   this._backoffTimes = this._setupBackoffTimes();
 
-  this._clientListeners = null;  // direct listeners from here to watchman.Client.
+  this._clientListeners = null; // direct listeners from here to watchman.Client.
 }
 
 // Define 'wildmatch' property, which must be available when we call the
-// WatchmanWatcher.createOptions() method. 
+// WatchmanWatcher.createOptions() method.
 Object.defineProperty(WatchmanClient.prototype, 'wildmatch', {
-  get() { return this._wildmatch; }
+  get() {
+    return this._wildmatch;
+  },
 });
 
 /**
@@ -56,16 +57,16 @@ Object.defineProperty(WatchmanClient.prototype, 'wildmatch', {
 WatchmanClient.prototype.subscribe = function(watchmanWatcher, root) {
   let subscription;
   let watcherInfo;
-  
+
   return this._setupClient()
     .then(() => {
       watcherInfo = this._createWatcherInfo(watchmanWatcher);
       subscription = watcherInfo.subscription;
       return this._watch(subscription, root);
     })
-    .then((resp) => this._clock(subscription))
-    .then((data) => this._subscribe(subscription));
-   // Note: callers are responsible for noting any subscription failure.
+    .then(() => this._clock(subscription))
+    .then(() => this._subscribe(subscription));
+  // Note: callers are responsible for noting any subscription failure.
 };
 
 /**
@@ -74,12 +75,14 @@ WatchmanClient.prototype.subscribe = function(watchmanWatcher, root) {
  * which will end the connection to the watchman.Client, too.
  */
 WatchmanClient.prototype.closeWatcher = function(watchmanWatcher) {
-  let watcherInfos = Object.values(this._watcherMap);
+  let watcherInfos = Object.keys(this._watcherMap).map(
+    key => this._watcherMap[key]
+  );
   let numWatchers = watcherInfos.length;
 
   if (numWatchers > 0) {
     let watcherInfo;
-    
+
     for (let info of watcherInfos) {
       if (info.watchmanWatcher === watchmanWatcher) {
         watcherInfo = info;
@@ -93,7 +96,7 @@ WatchmanClient.prototype.closeWatcher = function(watchmanWatcher) {
       numWatchers--;
 
       if (numWatchers === 0) {
-        this._clearLocalVars();  // nobody watching, so shut the watchman.Client down.
+        this._clearLocalVars(); // nobody watching, so shut the watchman.Client down.
       }
     }
   }
@@ -111,7 +114,7 @@ WatchmanClient.prototype._setupBackoffTimes = function() {
     _next: 0,
 
     next() {
-      let val = this._times[_next];
+      let val = this._times[this._next];
       if (this._next < this._times.length - 1) {
         this._next++;
       }
@@ -120,14 +123,14 @@ WatchmanClient.prototype._setupBackoffTimes = function() {
 
     reset() {
       this._next = 0;
-    }
-  }
-}
+    },
+  };
+};
 
 /**
  * Set up the connection to the watchman client. Return a promise
  * that is fulfilled when we have a client that has finished the
- * capabilityCheck. 
+ * capabilityCheck.
  */
 WatchmanClient.prototype._setupClient = function() {
   if (!this._clientPromise) {
@@ -148,9 +151,10 @@ WatchmanClient.prototype._setupClient = function() {
  */
 WatchmanClient.prototype._handleClientAndCheck = function(resolve, reject) {
   this._createClientAndCheck().then(
-    (value) => {
-      let {resp, client} = value;
-      
+    value => {
+      let resp = value.resp;
+      let client = value.client;
+
       try {
         this._wildmatch = resp.capabilities.wildmatch;
         this._relative_root = resp.capabilities.relative_root;
@@ -169,7 +173,7 @@ WatchmanClient.prototype._handleClientAndCheck = function(resolve, reject) {
         reject(error);
       }
     },
-    (error) => {
+    () => {
       // create & capability check failed in any of several ways,
       // do the retry with backoff.
 
@@ -187,7 +191,7 @@ WatchmanClient.prototype._handleClientAndCheck = function(resolve, reject) {
       }, backoffMillis);
     }
   );
-}
+};
 
 /**
  * Create a promise that will only be fulfilled when either
@@ -197,14 +201,15 @@ WatchmanClient.prototype._handleClientAndCheck = function(resolve, reject) {
  */
 WatchmanClient.prototype._createClientAndCheck = function() {
   return new Promise((resolve, reject) => {
-    
     let client;
-    
+
     try {
-      client = new watchman.Client(this._watchmanBinaryPath
-                                     ? { watchmanBinaryPath: this._watchmanBinaryPath}
-                                   : {});
-    } catch(error) {
+      client = new watchman.Client(
+        this._watchmanBinaryPath
+          ? { watchmanBinaryPath: this._watchmanBinaryPath }
+          : {}
+      );
+    } catch (error) {
       // if we're here, either the binary path is bad or something
       // else really bad happened. The client doesn't even attempt
       // to connect until we send it a command, though.
@@ -212,7 +217,7 @@ WatchmanClient.prototype._createClientAndCheck = function() {
       return;
     }
 
-    client.on('error', (error) => {
+    client.on('error', error => {
       client.removeAllListeners();
       reject(error);
     });
@@ -223,7 +228,7 @@ WatchmanClient.prototype._createClientAndCheck = function() {
     });
 
     client.capabilityCheck(
-      {optional: ['wildmatch', 'relative_root']},
+      { optional: ['wildmatch', 'relative_root'] },
       (error, resp) => {
         try {
           client.removeAllListeners();
@@ -231,7 +236,7 @@ WatchmanClient.prototype._createClientAndCheck = function() {
           if (error) {
             reject(error);
           } else {
-            resolve({resp, client});
+            resolve({ resp, client });
           }
         } catch (err) {
           // In case we get something weird in the block using 'resp'.
@@ -240,9 +245,9 @@ WatchmanClient.prototype._createClientAndCheck = function() {
           reject(err);
         }
       }
-    )
+    );
   });
-}
+};
 
 /**
  * Clear out local state at the beginning and if we end up
@@ -270,17 +275,17 @@ WatchmanClient.prototype._genSubscription = function() {
 };
 
 /**
- * Create a new watcherInfo entry for the given watchmanWatcher and 
+ * Create a new watcherInfo entry for the given watchmanWatcher and
  * initialize it.
  */
 WatchmanClient.prototype._createWatcherInfo = function(watchmanWatcher) {
   let watcherInfo = {
     subscription: this._genSubscription(),
-    watchmanWatcher: watchmanWatcher,           
-    root: null,                      // set during 'watch' or 'watch-project'
-    relativePath: null,              // same
-    since: null,                     // set during 'clock'
-    options: null                    // created and set during 'subscribe'.
+    watchmanWatcher: watchmanWatcher,
+    root: null, // set during 'watch' or 'watch-project'
+    relativePath: null, // same
+    since: null, // set during 'clock'
+    options: null, // created and set during 'subscribe'.
   };
 
   this._watcherMap[watcherInfo.subscription] = watcherInfo;
@@ -293,7 +298,7 @@ WatchmanClient.prototype._createWatcherInfo = function(watchmanWatcher) {
  */
 WatchmanClient.prototype._getWatcherInfo = function(subscription) {
   return this._watcherMap[subscription];
-}
+};
 
 /**
  * Given a watchmanWatcher and a root, issue the correct 'watch'
@@ -302,38 +307,35 @@ WatchmanClient.prototype._getWatcherInfo = function(subscription) {
  * of the 'watch' or 'watch-project' here.
  */
 WatchmanClient.prototype._watch = function(subscription, root) {
-  
   return new Promise((resolve, reject) => {
     let watcherInfo = this._getWatcherInfo(subscription);
 
-    if (this._relative_root) { 
-      this._client.command(['watch-project', root],
-                           (error, resp) => {
-                             if (error) {
-                               reject(error);
-                             } else {
-                               watcherInfo.root = resp.watch;
-                               watcherInfo.relativePath = resp.relative_path
-                                 ? resp.relative_path
-                                 : '';
-                               resolve(resp);
-                             }
-                           });
+    if (this._relative_root) {
+      this._client.command(['watch-project', root], (error, resp) => {
+        if (error) {
+          reject(error);
+        } else {
+          watcherInfo.root = resp.watch;
+          watcherInfo.relativePath = resp.relative_path
+            ? resp.relative_path
+            : '';
+          resolve(resp);
+        }
+      });
     } else {
-      this._client.command(['watch', root],
-                           (error, resp) => {
-                             if (error) {
-                               reject(error);
-                             } else {
-                               watcherInfo.root = root;
-                               watcherInfo.relativePath = '';
-                               resolve(resp);
-                             }
-                           });
+      this._client.command(['watch', root], (error, resp) => {
+        if (error) {
+          reject(error);
+        } else {
+          watcherInfo.root = root;
+          watcherInfo.relativePath = '';
+          resolve(resp);
+        }
+      });
     }
   });
 };
-  
+
 /**
  * Issue the 'clock' command to get the time value for use with the 'since'
  * option during 'subscribe'.
@@ -342,19 +344,18 @@ WatchmanClient.prototype._clock = function(subscription) {
   return new Promise((resolve, reject) => {
     let watcherInfo = this._getWatcherInfo(subscription);
 
-    this._client.command(['clock', watcherInfo.root],
-                         (error, resp) => {
-                           if (error) {
-                             reject(error);
-                           } else {
-                             watcherInfo.since = resp.clock;
-                             resolve(resp);
-                           }
-                         });
+    this._client.command(['clock', watcherInfo.root], (error, resp) => {
+      if (error) {
+        reject(error);
+      } else {
+        watcherInfo.since = resp.clock;
+        resolve(resp);
+      }
+    });
   });
 };
 
-/** 
+/**
  * Do the internal handling of calling the watchman.Client for
  * a subscription.
  */
@@ -365,9 +366,9 @@ WatchmanClient.prototype._subscribe = function(subscription) {
     // create the 'bare' options w/o 'since' or relative_root.
     // Store in watcherInfo for later use if we need to reset
     // things after an 'end' caught here.
-    let options = watcherInfo.watchmanWatcher.createOptions();  
+    let options = watcherInfo.watchmanWatcher.createOptions();
     watcherInfo.options = options;
-    
+
     // Dup the options object so we can add 'relative_root' and 'since'
     // and leave the original options object alone. We'll do this again
     // later if we need to resubscribe after 'end' and reconnect.
@@ -379,19 +380,21 @@ WatchmanClient.prototype._subscribe = function(subscription) {
 
     options.since = watcherInfo.since;
 
-    this._client.command(['subscribe', watcherInfo.root, subscription, options],
-                         (error, resp) => {
-                           if (error) {
-                             reject(error);
-                           } else {
-                             resolve(resp);
-                           }
-                         });
+    this._client.command(
+      ['subscribe', watcherInfo.root, subscription, options],
+      (error, resp) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(resp);
+        }
+      }
+    );
   });
-}
+};
 
 /**
- * Handle the 'subscription' (file change) event, by calling the 
+ * Handle the 'subscription' (file change) event, by calling the
  * handler on the relevant WatchmanWatcher.
  */
 WatchmanClient.prototype._onSubscription = function(resp) {
@@ -403,9 +406,12 @@ WatchmanClient.prototype._onSubscription = function(resp) {
     watcherInfo.watchmanWatcher.handleChangeEvent(resp);
   } else {
     // Note it in the log, but otherwise ignore it
-    console.error("WatchmanClient error - received 'subscription' event " +
-                  "for non-existent subscription '" +
-                  resp.subscription + "'");
+    console.error(
+      "WatchmanClient error - received 'subscription' event " +
+        "for non-existent subscription '" +
+        resp.subscription +
+        "'"
+    );
   }
 };
 
@@ -416,8 +422,9 @@ WatchmanClient.prototype._onSubscription = function(resp) {
  * which subscription it belonged to).
  */
 WatchmanClient.prototype._onError = function(error) {
-  Object.values(this._watcherMap).forEach((watcherInfo) =>
-                                          watcherInfo.watchmanWatcher.handleErrorEvent(error));
+  Object.values(this._watcherMap).forEach(watcherInfo =>
+    watcherInfo.watchmanWatcher.handleErrorEvent(error)
+  );
 };
 
 /**
@@ -429,47 +436,56 @@ WatchmanClient.prototype._onError = function(error) {
  * call the error handler on each existing WatchmanWatcher.
  */
 WatchmanClient.prototype._onEnd = function() {
-  console.warn('[sane.WatchmanClient] Warning: Lost connection to watchman, reconnecting..');
+  console.warn(
+    '[sane.WatchmanClient] Warning: Lost connection to watchman, reconnecting..'
+  );
 
   // Hold the old watcher map so we use it to recreate all subscriptions.
   let oldWatcherInfos = Object.values(this._watcherMap);
 
   this._clearLocalVars();
-  
-  this._setupClient()
-    .then(
-      (client) => {
-        let promises = oldWatcherInfos.map((watcherInfo) =>
-                                           this.subscribe(watcherInfo.watchmanWatcher,
-                                                          watcherInfo.watchmanWatcher.root));
-        Promise.all(promises)
-          .then(
-            (resultArray) => {
-              console.log('[sane.WatchmanClient]: Reconnected to watchman');
-            },
-            (error) => {
-              console.error("[sane.WatchmanClient]: Reconnected to watchman, but failed to " +
-                            "reestablish at least one subscription, cannot continue");
-              console.error(error);
-              oldWatcherInfos.forEach((watcherInfo) =>
-                                      watcherInfo.watchmanWatcher.handleErrorEvent(error));
-              // XXX not sure whether to clear all _watcherMap instances here,
-              // but basically this client is inconsistent now, since at least one
-              // subscribe failed. 
-            });
-      },
-      (error) => {
-        console.error('[sane.WatchmanClient]: Lost connection to watchman, ' +
-                      'reconnect failed, cannot continue');
-        console.error(error);
-        oldWatcherInfos.forEach((watcherInfo) =>
-                                watcherInfo.watchmanWatcher.handleErrorEvent(error));
-      });
+
+  this._setupClient().then(
+    () => {
+      let promises = oldWatcherInfos.map(watcherInfo =>
+        this.subscribe(
+          watcherInfo.watchmanWatcher,
+          watcherInfo.watchmanWatcher.root
+        )
+      );
+      Promise.all(promises).then(
+        () => {
+          console.log('[sane.WatchmanClient]: Reconnected to watchman');
+        },
+        error => {
+          console.error(
+            '[sane.WatchmanClient]: Reconnected to watchman, but failed to ' +
+              'reestablish at least one subscription, cannot continue'
+          );
+          console.error(error);
+          oldWatcherInfos.forEach(watcherInfo =>
+            watcherInfo.watchmanWatcher.handleErrorEvent(error)
+          );
+          // XXX not sure whether to clear all _watcherMap instances here,
+          // but basically this client is inconsistent now, since at least one
+          // subscribe failed.
+        }
+      );
+    },
+    error => {
+      console.error(
+        '[sane.WatchmanClient]: Lost connection to watchman, ' +
+          'reconnect failed, cannot continue'
+      );
+      console.error(error);
+      oldWatcherInfos.forEach(watcherInfo =>
+        watcherInfo.watchmanWatcher.handleErrorEvent(error)
+      );
+    }
+  );
 };
 
-
 module.exports = {
-
   /**
    * Create/retrieve an instance of the WatchmanClient. See the header comment
    * about the map of client instances, one per watchmanPath.
@@ -485,7 +501,7 @@ module.exports = {
     }
 
     if (watchmanBinaryPath == undefined || watchmanBinaryPath === null) {
-      watchmanBinaryPath = "";
+      watchmanBinaryPath = '';
     }
 
     let watchmanClient = clientMap[watchmanBinaryPath];
@@ -496,5 +512,5 @@ module.exports = {
     }
 
     return watchmanClient;
-  }
-}
+  },
+};
